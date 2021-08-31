08ff00715dcbea7a6477134ffeabf739d5f5e3b4
Swetha Gavini <swetha11895@gmail.com>
2021-08-31 18:20:45 -0400

    Update test_hello.py

--- test/test_hello.py
+++ test/test_hello.py
@@ -3,7 +3,7 @@
  from hello import hello
  class TestCuboid(unittest.TestCase):
     def test_volume(self):
-        self.assertAlmostEqual(cuboid_volume(2),8)
-        self.assertAlmostEqual(cuboid_volume(1),1)
-        self.assertAlmostEqual(cuboid_volume(0),0)
-        self.assertAlmostEqual(cuboid_volume(5.5),166.375)
+        self.assertAlmostEqual(cuboid(2),8)
+        self.assertAlmostEqual(cuboid(1),1)
+        self.assertAlmostEqual(cuboid(0),0)
+        self.assertAlmostEqual(cuboid(5.5),166.375)
