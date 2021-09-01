0d30ee6e5b407e7b897ba54fbc597c6544423f8a
krishnasaurabh <krishnasaurabh123@gmail.com>
2021-09-01 16:12:17 -0400

    Added sum and subtract functionality and its unit tests

--- code/hello.py
+++ code/hello.py
@@ -1,5 +1,14 @@
 def square(x):
     return x * x
 
+
 def cuboid(y):
     return (y*y*y)
+
+
+def sum(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
--- test/test_hello.py
+++ test/test_hello.py
@@ -1,8 +1,15 @@
 import unittest
-from code.hello import cuboid
+from code.hello import cuboid, sum, subtract, square
+
+
 class TestCuboid(unittest.TestCase):
     def test_volume(self):
-        self.assertAlmostEqual(cuboid(2),8)
-        self.assertAlmostEqual(cuboid(1),1)
-        self.assertAlmostEqual(cuboid(0),0)
-        self.assertAlmostEqual(cuboid(5.5),166.375)
+        self.assertAlmostEqual(cuboid(2), 8)
+        self.assertAlmostEqual(cuboid(1), 1)
+        self.assertAlmostEqual(cuboid(0), 0)
+        self.assertAlmostEqual(cuboid(5.5), 166.375)
+        self.assertAlmostEqual(sum(2, 3), 5)
+        self.assertAlmostEqual(subtract(5, 7), -2)
+        self.assertAlmostEqual(subtract(0, 0), 0)
+        self.assertAlmostEqual(square(0), 0)
+        self.assertAlmostEqual(square(-5), 25)
