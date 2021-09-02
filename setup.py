495c055bda0de2919f9ca8bb1345651ee9175788
krishnasaurabh <krishnasaurabh123@gmail.com>
2021-09-02 13:00:11 -0400

    added setup.py contents

--- setup.py
+++ setup.py
@@ -1,1 +1,28 @@
+from setuptools import setup
 
+setup(
+    name='Simple Calculator',
+    version='1.0',
+    description='This program does simple mathematical operations on given inputs.',
+    author='Krishna Saurabh Vankadaru',
+    author_email='kvankad@ncsu.edu',
+    url='https://github.com/ramyasaimullapudi/se21-hw2b',
+    packages=[],
+    long_description="""\
+        This program performs simple mathematical operations like
+        1. Addition
+        2. Subtract
+        3. Square
+        4. Cube
+      """,
+    classifiers=[
+        "License :: OSI Approved :: MIT",
+        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Data manipulation",
+    ],
+    keywords='',
+    license='MIT',
+    install_requires=[],
+)
